29bb84079993ee4d4c863aa3ace04539b29eab33
Irangal <88530878+Irangal@users.noreply.github.com>
2021-08-20 15:54:12 +0700

    Add files via upload

---
+++ Karakter.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+public class Karakter 
+{
+	private string name;
+	private int health;
+	private int damage;
+
+	public string Name 
+	{ get 
+		{ return name;
+		} set 
+		{ name = value;
+}
+} 
+ public int Health 
+{ get 
+	{ return health ;
+	}
+	set 
+		{ health = value ;
+		}
+	}
+	public int Damage 
+	{ get 
+		{ return damage; 
+		} set 
+		{ damage = value ;
+		}
+	}
+	public void Moveleft() 
+	{ 
+		Debug.Log (" Gerak ke kiri ");
+	}
+	public void Moveright()
+
+	{ Debug.Log (" Gerak ke Kanan ");
+	}
+	public void Jump ()
+
+	{ 
+		Debug.Log ("Loncat");
+	}
+	public void attack()
+	{ 
+		Debug.Log (" Serang ");
+	}
+}
